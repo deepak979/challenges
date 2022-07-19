902cddf59457d0535b34e3fdebb7458db8f12095
deepak979 <101112474+deepak979@users.noreply.github.com>
2022-07-19 17:53:39 +0530

    Create variables.tf

---
+++ challenge1/modules/main/variables.tf
@@ -0,0 +1,21 @@
+variable "location"{
+    description = "location where resource will be deployed"
+}
+variable "resource_group_name"{}
+variable "tags"{}
+variable "frontend_subnet"{}
+variable "midtier_subnet"{}
+variable "backend_subnet"{}
+variable "frontend_vm_size"{}
+variable "midtier_vm_size"{}
+variable "backend_vm_size"{}
+variable "frontend_vm_name"{}
+variable "midtier_vm_name"{}
+variable "backend_vm_name"{}
+variable "sql_password"{}
+variable "windows_password"{}
+variable "admin_username"{}
+variable "vm_publisher"{}
+variable "vm_offer"{}
+variable "vm_sku"{}
+variable "sql_admin"{}
