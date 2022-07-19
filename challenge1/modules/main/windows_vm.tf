a3da10b5c18c99f363d8743ee1077f9d7ee5a848
deepak979 <101112474+deepak979@users.noreply.github.com>
2022-07-19 17:53:10 +0530

    Update and rename windows_vm.tf.txt to windows_vm.tf

---
+++ challenge1/modules/main/windows_vm.tf
@@ -0,0 +1,79 @@
+#Creating frontend VM for hoting website
+
+resource "azurerm_network_interface" "frontend-nic" {
+  name                = "frontend-nic"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = var.frontend_subnet
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+resource "azurerm_windows_virtual_machine" "frontend" {
+  name                = var.frontend_vm_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  size                = var.frontend_vm_size
+  admin_username      = var.admin_username
+  admin_password      = var.windows_password 
+  network_interface_ids = [
+    azurerm_network_interface.frontend-nic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    disk_size_gb         = "128"
+  }
+
+  source_image_reference {
+    publisher = var.vm_publisher
+    offer     = var.vm_offer
+    sku       = var.vm_sku
+  }
+  depends_on = [
+    azurerm_network_interface.frontend-nic
+  ]
+}
+
+#Creating midtier VM for running different jobs
+
+resource "azurerm_network_interface" "midtier-nic" {
+  name                = "midtier-nic"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = var.midtier_subnet
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+resource "azurerm_windows_virtual_machine" "midtier" {
+  name                = var.midtier_vm_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  size                = var.midtier_vm_size
+  admin_username      = var.admin_username
+  admin_password      = var.windows_password 
+  network_interface_ids = [
+    azurerm_network_interface.midtier-nic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    disk_size_gb         = "128"
+  }
+
+  source_image_reference {
+    publisher = var.vm_publisher
+    offer     = var.vm_offer
+    sku       = var.vm_sku
+  }
+  depends_on = [
+    azurerm_network_interface.midtier-nic
+  ]
+}
